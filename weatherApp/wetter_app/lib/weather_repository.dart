5ceb1274f400643fd9409edc871f8abcebca728c
Muhammad <muhannmmad@gmail.com>
2024-09-30 08:39:14 +0200

    edited

--- weatherApp/wetter_app/lib/weather_repository.dart
+++ weatherApp/wetter_app/lib/weather_repository.dart
@@ -1,13 +1,13 @@
 import 'weather_data.dart';
 
 class WeatherRepository {
-  final WeatherData _weather = WeatherData(
+  final WeatherData weather = WeatherData(
     city: 'Tübingen',
     temperature: 18,
     weatherCondition: 'Rains',
   );
 
   WeatherData getWeather() {
-    return _weather;
+    return weather;
   }
 }
